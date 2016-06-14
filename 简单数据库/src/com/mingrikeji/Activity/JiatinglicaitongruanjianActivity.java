29a5fcb9b52892c17937f030df73d15fd785f621
coderly90controls <1499586287@qq.com>
2016-06-15 00:24:01 +0800

    试试啊
    添加了一个小项目

---
+++ 简单数据库/src/com/mingrikeji/Activity/JiatinglicaitongruanjianActivity.java
@@ -0,0 +1,11 @@
+package com.mingrikeji.Activity;
+import android.app.Activity;
+import android.os.Bundle;
+public class JiatinglicaitongruanjianActivity extends Activity {
+    private Object R;//private Object
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);   
+    }
+}
